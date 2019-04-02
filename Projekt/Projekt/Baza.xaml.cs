3429e6da92635ab48cded9035ca02a76253d450d
maniok112 <summerballowski@gmail.com>
2019-04-03 00:04:59 +0200

    +
    +

---
+++ Projekt/Projekt/Baza.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Projekt
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Baza.xaml
+    /// </summary>
+    public partial class Baza : Window
+    {
+        public Baza()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSprawdz_Click(object sender, RoutedEventArgs e)
+        {
+            // Tutaj Dodadać Sql Connection i Wyswietlać w DataGrind
+        }
+
+        private void Btnpowrot_Click(object sender, RoutedEventArgs e)
+        {
+            Menu form = new Menu();
+            form.Show();
+            this.Hide();
+        }
+    }
+}
